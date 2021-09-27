e08809cc1f7d211a1f273ff7ed53a050123104f3
Angelier Ramirez <aramirez@midasconsultores.com.ar>
2021-09-27 15:24:12 -0300

    Add project files.

---
+++ CoreDockerWebDemo2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDockerWebDemo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
